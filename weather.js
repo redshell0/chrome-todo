40d828e867a7ff269ae4cfdeb1c04b68a384c442
wungnim <wungnim@wungnims-MacBook-Pro.local>
2020-07-26 15:42:30 +0900

    first commit

---
+++ weather.js
@@ -0,0 +1,27 @@
+const COORDS = "coords";
+
+function handleGeoSuccess(position) {
+  console.log(position);
+}
+
+function handleGeoError() {
+  console.log("Cant");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
