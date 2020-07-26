deb9a7f1f02721a483815e2b297a04e70cededa6
redshell <baseball0020@gmail.com>
2020-07-26 15:52:34 +0900

    modify

--- weather.js
+++ weather.js
@@ -1,7 +1,17 @@
 const COORDS = "coords";
 
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
 function handleGeoSuccess(position) {
-  console.log(position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
 }
 
 function handleGeoError() {
