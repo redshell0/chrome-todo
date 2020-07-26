05a8b8de13a48313dc9e54a3788c3fde849136c8
wungnim <wungnim@wungnims-MacBook-Pro.local>
2020-07-26 16:16:55 +0900

    api

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "MY_API_KEY";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +30,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,6 +46,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
